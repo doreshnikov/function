55b7229e7396c9c4f955fda9d04c3cc8c55a9044
doreshnikov <dan.io.oreshnikov@gmail.com>
2019-01-26 18:02:30 +0300

    namspace removed

--- function.hpp
+++ function.hpp
@@ -5,50 +5,46 @@
 #include <cstddef>
 #include <memory>
 
-namespace doreshnikov {
+template<typename>
+class function;
 
-    template<typename>
-    class function;
+template<typename ReturnType, typename ...ArgumentTypes>
+class function<ReturnType(ArgumentTypes...)> {
 
-    template<typename ReturnType, typename ...ArgumentTypes>
-    class function<ReturnType(ArgumentTypes...)> {
+public:
+    function() : invoker() {};
+    ~function() = default;
 
+    template<typename CallableType>
+    explicit function(CallableType callable_object) : invoker(new callable_holder<CallableType>(callable_object)) {}
+
+    ReturnType operator()(ArgumentTypes... args) {
+        return invoker->invoke(args...);
+    }
+
+private:
+
+    class callable_base {
     public:
-        function() : invoker() {};
-        ~function() = default;
-
-        template<typename CallableType>
-        explicit function(CallableType callable_object) : invoker(new callable_holder<CallableType>(callable_object)) {}
-
-        ReturnType operator()(ArgumentTypes... args) {
-            return invoker->invoke(args...);
-        }
-
-    private:
-
-        class callable_base {
-        public:
-            callable_base() = default;
-            virtual ~callable_base() = default;
-            virtual ReturnType invoke(ArgumentTypes... args) = 0;
-        };
-
-        template<typename CallableType>
-        class callable_holder : public callable_base {
-        public:
-            explicit callable_holder(CallableType callable_object) : callable_base(),
-                                                                     _callable_object(callable_object) {}
-            ReturnType invoke(ArgumentTypes... args) {
-                return _callable_object(args...);
-            }
-        private:
-            CallableType _callable_object;
-        };
-
-        std::shared_ptr<callable_base> invoker;
-
+        callable_base() = default;
+        virtual ~callable_base() = default;
+        virtual ReturnType invoke(ArgumentTypes... args) = 0;
     };
 
-}
+    template<typename CallableType>
+    class callable_holder : public callable_base {
+    public:
+        explicit callable_holder(CallableType callable_object) : callable_base(),
+                                                                 _callable_object(callable_object) {}
+        ReturnType invoke(ArgumentTypes... args) {
+            return _callable_object(args...);
+        }
+    private:
+        CallableType _callable_object;
+    };
+
+    std::shared_ptr<callable_base> invoker;
+
+};
 
 #endif // FUNCTION_HPP
